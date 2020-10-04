a1d7a4c6acb035fb34f60f6db6fd6bbbc4aad4a4
Alison Machado <alison.sm92@gmail.com>
2020-10-03 22:53:33 -0300

    Fix DEFAULT_OUTPUT_DIR

--- config/constants.js
+++ config/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   FILE_PATTERN: '/**/*.js',
   DEFAULT_TITLE: 'complexity-inspector',
-  DEFAULT_OUTPUT_DIR: '/reports',
+  DEFAULT_OUTPUT_DIR: 'reports',
   DEFAULT_HEALTHY_SCORE: 60,
 };
