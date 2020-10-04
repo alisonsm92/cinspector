da25735c915b03c0fd62ce8a81daebfc9bb2b13e
Alison Machado <alison.sm92@gmail.com>
2020-10-04 00:45:23 -0300

    Fix tests

--- utils/platoOptionsAdapter.js
+++ utils/platoOptionsAdapter.js
@@ -6,16 +6,20 @@
  * Adapts CLI args to Plato options
  * @param {Args} args CLI args
  */
-const adapt = (args) => ({
-  src: args._[0],
-  dir: args.dir,
-  recurse: args.recurse,
-  exclude: args.exclude,
-  title: args.title,
-  quiet: args.quiet,
-  noempty: !args.empty,
-  eslint: args.eslint,
-  jshint: args.jshint,
-});
+const adapt = (args) => {
+  const [src] = args._ || [];
+
+  return {
+    src,
+    dir: args.dir,
+    recurse: args.recurse,
+    exclude: args.exclude,
+    title: args.title,
+    quiet: args.quiet,
+    noempty: !args.empty,
+    eslint: args.eslint,
+    jshint: args.jshint,
+  };
+};
 
 module.exports = { adapt };
