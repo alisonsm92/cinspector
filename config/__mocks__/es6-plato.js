b1f5ccc8062077bfab7a7ba13cfa52bddc0b4eda
Alison Machado <alison.sm92@gmail.com>
2020-10-06 23:13:10 -0300

    Fix tests

--- config/__mocks__/es6-plato.js
+++ config/__mocks__/es6-plato.js
@@ -1,5 +1,6 @@
 const plato = jest.createMockFromModule('es6-plato');
 
 plato.inspect = (filePattern, outputDir, options, done) => done();
+plato.getOverviewReport = () => ({ summary: { average: {}, total: {} } });
 
 module.exports = plato;
